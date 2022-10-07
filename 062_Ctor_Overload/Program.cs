d9c8c31f742a3d768ee87fba6d7a18b43fa19ebd
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-10-07 18:13:02 +0300

    Ctor overload

---
+++ 062_Ctor_Overload/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _062_Ctor_Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // school nesnesi School class'ının parametre almayan ctor'u üzerinden üretildi !
+            
+            School school = new School();
+            school.No = 302;
+            school.Kapasite = 13;
+
+            // school2 nesnesi 1 tane parametre alan ctor üzerinden üretildi!
+
+            School school2 = new School(303);
+            school2.Kapasite = 14;
+
+            // school3 nesnesi 2 tane parametre alan ctor üzerinden üretildi!
+            // No ve Kapasite bilgileri (301,10) nesne üretilirken parametre olarak ctor'a gönderildi!
+
+            School school3 = new School(301,10);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 062_Ctor_Overload/School.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _062_Ctor_Overload
+{
+
+    class School
+    {
+        public int No { get; set; }
+        public int Kapasite { get; set; }
+
+
+        public School()
+        {
+            Console.WriteLine("Parametre almayan ctor çalıştı...");
+        }
+
+        // Ctor'da bir method olduğu için methodlarda yapılan aşırı yükleme(overload) yöntemi kullanılarak birden fazla ctor versiyonu yazılabilir!
+
+        public School(int no)
+
+        {
+            No = no;
+            Console.WriteLine("Bir parametre alan ctor çalıştı...");
+        }
+
+        public School(int no , int kapasite)
+
+        {
+            No = no;
+            Kapasite = kapasite;
+            Console.WriteLine("İki parametre alan ctor çalıştı...");
+        }
+    }
+}
+
