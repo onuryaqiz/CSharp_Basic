07ec8f76591bea9215b2e7c7dc097a0805f7e699
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-10-07 17:53:31 +0300

    Ctor Parameter

---
+++ 061_Ctor_MandatoryParameter/Program.cs
@@ -0,0 +1,22 @@
+﻿using _02_Ctor_MandatoryParameter;
+
+namespace _061_Ctor_MandatoryParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Student class'ının ctor'u parametre aldığı için , parametreleri göndermeden nesne üretemeyiz! Aşağıdaki kod bloğu derleme esnasında hata alır!
+
+            //Student student = new Student();
+
+            Student student = new Student("Onur", "Yağız", "Ali");
+
+            // Ctor'da zorunlu olmayan alanlar daha sonra set ediliyor.
+            student.DogumYeri = "Ankara";
+            student.Yas = 12;
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 061_Ctor_MandatoryParameter/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Ctor_MandatoryParameter
+{
+    class Student
+    {
+        public string AdSoyad { get; set; }
+        public string Sinif { get; set; }
+        public string Telefon { get; set; }
+        public string DogumYeri { get; set; }
+        public int Yas { get; set; }
+
+       // ctor parametre alabilir , parametre alan bir ctor yazıldığı taktirde nesne üretilirken bu parametrelerin gönderilmesi zorunlu hale gelir.
+       // Bir class'dan nesne üretilirken zorunlu olmasını istediğimiz bazı değerlerin girildiğini garanti altına almak için bu yöntem kullanılabilir.
+       // Aşağıdaki ctor ile AdSoyad,Sinif ve Telefon değerlerinin nesne üretilirken girilmesi garanti altına alınmıştır!
+
+        public Student(string adSoyad , string sinif, string telefon)
+        {
+            // Parametre olarak gelen değerleri nesnenin ilgili propety'lerine aktarıyoruz.
+            AdSoyad = adSoyad;
+            Sinif = sinif;
+            Telefon = telefon;
+        }
+
+        // Bir class'tan nesne üretilmesini engellemek için ctor'un erişim belirleyicisi private olarak belirlenebilir.
+        // ctor private yapıldığı taktirde yalnızca tanımlandığı tip tarafından örneklenebilir!
+        //private Student()
+        //{
+
+        //}
+    }
+}
