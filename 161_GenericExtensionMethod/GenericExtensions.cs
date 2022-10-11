857f4a62baf3fe5ac6cf10493949012b462d1d73
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-10-11 20:00:00 +0300

    GenericExtensionMethod

---
+++ 161_GenericExtensionMethod/GenericExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _161_GenericExtensionMethod
+{
+    public static class GenericExtensions
+    {
+        // Sadece int değişkenlerde çıkacak method
+        public static int KareAl(this int sayi)
+        {
+            return sayi * sayi;
+        }
+
+       // Tüm .NET nesnelerinde çıkacak method
+        public static void HerYerdeCikacakMethod(this object nesne)
+        {
+
+        }
+
+        // Sadece Struct nesnelerde çıkacak method
+        public static void SadeceStruct<T>(this T obj) where T :struct
+        {
+
+        }
+
+        // Sadece urun nesnelerinde çıkacak method 
+        public static void SadeceUrun<T>(this T urun) where T:Urun
+        {
+
+        }
+    }
+}
---
+++ 161_GenericExtensionMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _161_GenericExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 10;
+            sayi.KareAl();
+            sayi.HerYerdeCikacakMethod();
+            sayi.SadeceStruct<int>(); // Generic tipi belirtmesek de olur .
+    
+            StructTest st=new StructTest();
+
+            st.HerYerdeCikacakMethod();
+            st.SadeceStruct();
+
+            Urun urun = new Urun();
+            urun.HerYerdeCikacakMethod();
+            urun.SadeceUrun();
+
+            Console.ReadKey();        
+        
+        }
+    }
+
+
+    public class Urun
+    {
+    }
+
+    struct StructTest
+    {
+
+
+    }
+}
