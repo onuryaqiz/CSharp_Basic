f499a1c63c0a4f8ba682914fd12fef665e21018c
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-10-22 19:19:05 +0300

    Non-Static

---
+++ 17_NonStatic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_NonStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun = new Urun(10, "Klavye", 100);
+            Console.WriteLine(urun.Info());
+
+            // Aşağıdaki üyeler non-static olduğu için urun nesnesi üzerinden erişilebilirler.
+            urun.Id = 10;
+            urun.Name = "Klavye";
+            urun.Price = 100;
+            urun.Info();
+
+            // Urun class'ının içindeki üyeler non-static olduğu için aşağıdaki gibi Urun class'ı üzerinden erişilemez!
+            //Urun.Id = 10;
+            //Urun.Name = "Test";
+            //Urun.Price = 1000;
+            //Urun.Info();
+
+            urun.ZamYap(0.10); // 0.10 yerine .10 yazabiliriz ikisi de aynı anlama gelir.
+
+            Console.WriteLine(urun.Info(urun));
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 17_NonStatic/Urun.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_NonStatic
+{
+     class Urun
+    {
+        // Bir üyeye static keyword'ü uygulanmadıkça o üye nonstatic'tir ve bir nesneye aittir.
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public Urun (int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public string Info()
+        {
+            return string.Format("Id:{0} Name:{1} Price{2}", Id, Name, Price);
+        }
+
+        public void ZamYap(double oran)
+        {
+            Price += Price * oran;
+            // this.Price+=Price*oran; üsttekiyle aynı işi yapar , this keyword'ünü kullanmasak da olur.
+
+            // this keyword'ü non-static bir üyenin hizmet ettiği nesne örneğini getirir.
+            Urun yeniUrun = this;
+            yeniUrun.Price = 10000;
+
+            // Üretilen nesneyi başka bir iş parçasına(başka bir method'a ) gönderdik.
+            Info(this); 
+        }
+
+        public string Info(Urun urun)
+        {
+            return string.Format("Id: {0} Ad: {1} Fiyat: {2}", urun.Id, urun.Name, urun.Price);
+        }
+    }
+}
