1cd64ef3a21487c5312f2d0873e7506f2ced0343
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-11-17 11:46:21 +0300

    Encapsulation

---
+++ 01_Encapsulation/Order.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Encapsulation
+{
+    public class Order
+    {
+        // Id ve price field'ları private olduğu için nesne kullanıcı bu field'lara direkt erişim sağlayamaz. Erişim sağlamak için aşağıda tanımlanan public durumdaki kapsül methodlar kullanılmak zorundadır.
+        // Kapsül methodların görevi private field'lara yapılan erişimleri kontrol altına alarak değerlerin kontrollü şekilde okunmasını ve yazılmasını sağlamaktır. 
+        
+        private int id;
+        private int price;
+    
+        // get_price methodunun amacı sadece private bir field olan price'ın sakladığı değeri okumak(nesne kullanıcına iletmek)dir.
+
+        public int get_price()
+        {
+            return price;
+        }
+
+        // set_price methodunun amacı nesne kullanıcısının private durumdaki field'ına kontrollü bir şekilde değer atanmasını sağlamaktır. Bu örnekte uyguladığımız validasyon ile price değerinin 0 girilmesini garanti altına almış olduk. Böylelikle 0 tutarlı bir sipariş oluşturulmayacak . 
+        public void set_price(int newPrice)
+        {
+            if (newPrice == 0)
+                throw new ArgumentException("Geçersiz bir fiyat girdiniz ! Fiyat sıfırdan büyük olmalıdır!");
+            else if (newPrice == price)
+                throw new ArgumentException("Daha önceden tanımlı olan fiyat ile aynı değeri girdiniz! Lütfen farklı bir değer giriniz!");
+            else if(newPrice>0)
+                price = newPrice;
+
+        }
+
+        public int get_id()
+        {
+            return id;
+        }
+
+        public void set_id(int newId)
+        {
+            if (newId > 0 && newId < 1000)
+                id = newId;
+            else
+                throw new ArgumentException("Id değeri 0-1000 arasında olmalıdır!");
+        }
+
+    }
+}
---
+++ 01_Encapsulation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+
+            // 1000'den büyük id'li bir sipariş nesnesi oluşturulamaz ! Aşağıdaki satır çalışma zamanında hata verir.
+            // order.set_id(1500);
+
+            order.set_id(500);
+
+            // id değerini çekiyoruz(get işlemi) En son 500 değeri atandığı için ekrana 500 yazar.
+            Console.WriteLine(order.get_id());
+
+            try
+            {
+                // ilk değer belirlendi
+                order.set_price(10);
+                Console.WriteLine("Fiyat giriniz: ");
+                int newPrice = Convert.ToInt32(Console.ReadLine());
+
+                order.set_price(newPrice);
+                Console.WriteLine("Güncel fiyat: {0}",order.get_price());
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.ForegroundColor=ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+           Console.ReadKey();
+        }
+    }
+}
