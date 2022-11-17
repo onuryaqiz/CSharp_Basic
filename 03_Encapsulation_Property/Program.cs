4be9082138cc401e2f6d5111c172274a2f33a613
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-11-17 15:56:27 +0300

    PropertyTest

---
+++ 03_Encapsulation_Property/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Encapsulation_Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           PropertyTest pt = new PropertyTest();
+
+            int test1 = pt.SadeceGetEdilebilirAlan;        // get işlemi gerçekleştirilebilir.
+            //pt.SadeceGetEdilebilirAlan=30;               // set işlemi gerçekleştirilemez!
+                                                           
+            //int test2 = pt.SadeceSetEdilebilirAlan;      // get işlemi gerçekleştirilemez!
+            pt.SadeceSetEdilebilirAlan = 10;               // set işlemi gerçekleştirilebilir.
+
+            int test3 = pt.SetBloguPrivateAlan;            // get işlemi gerçekleştirilebilir.
+            //pt.SetBloguPrivateAlan = 10;                 // set işlemi gerçekleştirilemez! 
+
+            double test4 = pt.HesaplananAlan;              // get işlemi gerçekleştirilebilir.
+            //pt.HesaplananAlan=10;                        // set işlemi gerçekleştirilemez! 
+                                 
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 03_Encapsulation_Property/PropertyTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Encapsulation_Property
+{
+    class PropertyTest
+    {
+        int sadeceGetEdilebilirAlan = 10; // sadece get işlemi yapılabildiği için default bir değer ataması yaptık.
+        int sadeceSetEdilebilirAlan;
+        int setBloguPrivateAlan;
+
+        public int SadeceGetEdilebilirAlan
+        {
+            get { return sadeceGetEdilebilirAlan; }
+        }
+
+        public int SadeceSetEdilebilirAlan
+        {
+            set { sadeceSetEdilebilirAlan = value; }
+        }
+
+        public int SetBloguPrivateAlan
+        {
+            get { return setBloguPrivateAlan; }
+
+            // Set bloğu private olduğundan dolayı , property üzerinden yapılan atama işlemleri yalnızca bu sınıf tarafından gerçekleştirilebilir.
+            private set { setBloguPrivateAlan = value; }
+        }
+
+        // Kendi içerisinde bir hesaplama yapıp sonucunu dönen bir property tasarlayabiliriz. Ancak bu property'nin private bir field'ı olmadığından sadece get işlemi yapabilir.
+        public double HesaplananAlan
+        {
+            get { return sadeceGetEdilebilirAlan + sadeceSetEdilebilirAlan / 2; }
+        }
+
+        // Aynı class içerisinde olduğu iin aşağıdaki method ile private olan set bloğuna set işlemi gerçekleştirebiliriz.
+        void SetBloguPrivateAlanTest()
+        {
+            SetBloguPrivateAlan = 10;
+        }
+        // Sadece Get bloğu olan property'lere bu şekilde class'ın içerisindeki bir method ile değer ataması yapılabilir.
+        void SadeceGetEdilebilirAlanTest()
+        {
+            // SadeceGetEdilebilirAlan=10;
+            sadeceGetEdilebilirAlan = 20;
+        }
+    }
+}
