cc4cef73d50c9f148802d0c58a67851c25531009
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-11-18 12:24:17 +0300

    Auto Property

---
+++ 04_Auto_Property/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Auto_Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun = new Urun();
+
+            urun.Id = 10;
+            urun.UrunAdi = "Laptop";
+            urun.Fiyat = 6000;
+            urun.Kdv = 0.18;
+            // urun.SadeceGet=10; // Sadece get işlemi uygulanabilir, set uygulanamaz!
+
+            Console.WriteLine("Id:{0}\nAdi: {1}\nFiyat: {2}\nKdv : {3}\nSadeceGet: {4}",urun.Id,urun.UrunAdi,urun.Fiyat,urun.Kdv,urun.SadeceGet);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 04_Auto_Property/Urun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Auto_Property
+{
+    // Auto property'ler sadece veri taşıma işlemleri için kullanılır , herhangi bir validasyon ya da encapsülation kullanmazlar bu yönleriyle field'larla blokları olduğu için gerek duyulduğunda genişletilip encpsülation ve validasyon uygulanabilir hale getirilebilirler.
+    class Urun
+    {
+        // Auto property kullanım şekli 
+        public int Id { get; set; }
+
+        public string UrunAdi { get; set; }
+
+        public double Fiyat { get; set; }
+
+        public double Kdv { get; set; }
+
+        public double SadeceGet { get;  }
+    }
+}
